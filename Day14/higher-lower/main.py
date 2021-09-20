59233e91a432bcf9c6780c536faa7c80ac2537a8
Stanislav Rakitov <prosperousrf@gmail.com>
2021-09-20 11:12:16 +0300

    higher-lower

--- Day14/higher-lower/main.py
+++ Day14/higher-lower/main.py
@@ -1,21 +1,65 @@
-# Generate a random account from the game data.
+from game_data import data
+import random
+from art import logo, vs
+from replit import clear
 
-# Format account data into printable format.
 
-# Ask user for a guess.
+def get_random_account():
+    """Get data from random account"""
+    return random.choice(data)
 
-# Check if user is correct.
-## Get follower count.
-## If Statement
 
-# Feedback.
+def format_data(account):
+    """Format account into printable format: name, description and country"""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    # print(f'{name}: {account["follower_count"]}')
+    return f"{name}, a {description}, from {country}"
 
-# Score Keeping.
 
-# Make game repeatable.
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess
+    and returns True if they got it right.
+    Or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
-# Make B become the next A.
 
-# Add art.
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
 
-# Clear screen between rounds.
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        clear()
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
+
+
+game()
+
