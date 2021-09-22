30ab091830f940b1dae9133f77f3f9a7896e0f72
Stanislav Rakitov <prosperousrf@gmail.com>
2021-09-22 16:19:18 +0300

    Day16 modules

---
+++ Day16/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+from prettytable import PrettyTable
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+table = PrettyTable()
+table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+print(table)
