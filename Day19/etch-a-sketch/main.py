532a9767db2c3b3c44de08349be93a57d28d5a94
Stanislav Rakitov <prosperousrf@gmail.com>
2021-09-25 15:57:54 +0300

    Day19. etch-a-sketch end

--- Day19/etch-a-sketch/main.py
+++ Day19/etch-a-sketch/main.py
@@ -8,6 +8,32 @@ def move_forwards():
     tim.forward(10)
 
 
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "Up")
+screen.onkey(move_backwards, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkey(clear, "c")
+
 screen.exitonclick()
