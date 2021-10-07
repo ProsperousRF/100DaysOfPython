dd159edfb44434f970a7dd25b503f5832a032d55
Stanislav Rakitov <prosperousrf@gmail.com>
2021-10-07 15:25:13 +0300

    day-26-2-exercise

--- Day26/day-26-2-exercise/main.py
+++ Day26/day-26-2-exercise/main.py
@@ -1,11 +1,10 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 # 🚨 Do Not Change the code above
 
-#Write your 1 line code 👇 below:
+# Write your 1 line code 👇 below:
 
+result = [num for num in numbers if num % 2 == 0]
 
-
-#Write your code 👆 above:
+# Write your code 👆 above:
 
 print(result)
-
