9e9ce2487834d268d822a229335899168c5a6a97
Stanislav Rakitov <rakitov.s@gmail.com>
2021-10-17 15:51:23 +0300

    day-26-end

--- Day26/day-26-5-exercise/main.py
+++ Day26/day-26-5-exercise/main.py
@@ -1,16 +1,34 @@
-weather_c = {
-    "Monday": 12,
-    "Tuesday": 14,
-    "Wednesday": 15,
-    "Thursday": 14,
-    "Friday": 21,
-    "Saturday": 22,
-    "Sunday": 24,
+#For Loop
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
+
+#List Comprehension
+new_list = [n + 1 for n in numbers]
+
+#String as List
+name = "Angela"
+letters_list = [letter for letter in name]
+
+#Range as List
+range_list = [n * 2 for n in range(1, 5)]
+
+#Conditional List Comprenhension
+names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+
+upper_case_names = [name.upper() for name in names if len(name) > 4]
+
+#Dictionary Comprehension
+import random
+student_grades = {name: random.randint(1, 100) for name in names}
+print(student_grades)
+
+passed_students = {
+    student: grade
+    for (student, grade) in student_grades.items() if grade >= 60
 }
-# 🚨 Don't change code above 👆
+print(passed_students)
 
-
-# Write your code 👇 below:
-weather_f = {day: temp * 9 / 5 + 32 for (day, temp) in weather_c.items()}
-
-print(weather_f)
